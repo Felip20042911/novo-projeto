5ea8a5478c1ea977ebbdfeb64fb97d1e9aac5cea
Felip20042911 <woichik1709@gmail.com>
2021-07-22 16:51:38 -0300

    somente os valores de dolar e euro

--- web.py
+++ web.py
@@ -2,26 +2,35 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-web = webdriver.Chrome()
-web.get('https://web.whatsapp.com/')
+def abrir_pagina():
+    web.get('https://www.google.com')
 
-sleep(20)
-
-nomes = ['Nel', 'Eu']
-
-mensagem = 'oiie, tudo bom?'
-
+def abrir_whats():
+    web.get('https://web.whatsapp.com/')
+    sleep(20)
 
 def pesquisa_nome(nome):
     web.find_element_by_xpath('//*[@id="side"]/div[1]/div/label/div/div[2]').send_keys(nome)
     web.find_element_by_xpath('//*[@id="side"]/div[1]/div/label/div/div[2]').send_keys(Keys.ENTER)
-
 
 def mandar_msg(msg):
     web.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[1]/div/div[2]').send_keys(msg)
     web.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[1]/div/div[2]').send_keys(Keys.ENTER)
 
 
-for nome in nomes:
-    pesquisa_nome(nome)
-    mandar_msg(mensagem)
+web = webdriver.Chrome()
+#pegando os valores
+
+abrir_pagina()
+    
+web.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('Dólar')
+web.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+dolar = web.find_element_by_xpath('<span class="DFlfde SwHCTb" data-precision="2" data-value="5.2002">5,20</span>').get_attribute('data-value')
+print(dolar)
+
+abrir_pagina()
+
+web.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('Euro')
+web.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
+euro = web.find_element_by_xpath('//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]').get_attribute('data-value')
+print(euro)
